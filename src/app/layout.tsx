423d37d216430cfc40b13de174b3a5138270528e
NineSirius <ruslanbulah3@gmail.com>
2023-06-06 10:24:38 +0600

    add nunito font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Nunito } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const nunito = Nunito({ subsets: ['latin'] })
 
 export const metadata = {
     title: 'Create Next App',
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="ru">
-            <body className={inter.cl assName}>{children}</body>
+            <body className={nunito.className}>{children}</body>
         </html>
     )
 }
