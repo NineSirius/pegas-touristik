917c034a88118fd05ad310290f6543fa7682801f
k1xd-fr <117137914+k1xd-fr@users.noreply.github.com>
2023-06-06 15:34:51 +0600

    fix

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -81,7 +81,7 @@ const Contact = () => {
       <iframe
         className={style.container__map}
         src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d11701.382479784783!2d74.55804495!3d42.84443485!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sgoogle%20map!5e0!3m2!1sru!2skg!4v1686035319458!5m2!1sru!2skg"
-        // style="border:0;"
+        height={692}
         allowFullScreen={false}
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
